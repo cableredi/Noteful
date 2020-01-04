b35361cf10e1a8f485d9a3cf2077fea3dc88b815
Kimberly Cable <cableredi@comcast.net>
2020-01-03 17:14:57 -0700

    added now.json

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  //API_ENDPOINT_FOLDERS: 'http://localhost:9090/folders',
-  //API_ENDPOINT_NOTES: 'http://localhost:9090/notes',
-  API_ENDPOINT_FOLDERS: 'http://localhost:8000/api/folders',
-  API_ENDPOINT_NOTES: 'http://localhost:8000/api/notes',
+  //API_ENDPOINT_FOLDERS: 'http://localhost:8000/api/folders',
+  //API_ENDPOINT_NOTES: 'http://localhost:8000/api/notes',
+  API_ENDPOINT_FOLDERS: 'https://enigmatic-harbor-00438.herokuapp.com/api/folders',
+  API_ENDPOINT_NOTES: 'https://enigmatic-harbor-00438.herokuapp.com/api/notes',
 }
